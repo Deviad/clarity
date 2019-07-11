baa343c74b8f9ccb69a3712a4e43cadbdca9a8b7
Davide Pugliese <deviad@gmail.com>
2019-07-11 21:42:39 +0300

    Add configuration server

---
+++ clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/router/EthereumRouter.java
@@ -0,0 +1,22 @@
+package com.clarity.transactiondispatcher.router;
+
+import com.clarity.transactiondispatcher.handler.EthereumHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Configuration
+public class EthereumRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(EthereumHandler ethereumHandler) {
+        return RouterFunctions.route(GET("/ethtransaction/{id}").and(accept(APPLICATION_JSON)), ethereumHandler::readTransaction)
+                .andRoute(POST("/ethtransaction").and(accept(APPLICATION_JSON)).and(contentType(APPLICATION_JSON)), ethereumHandler::createTransaction)
+                .andRoute(GET("/ethtransaction").and(accept(APPLICATION_JSON)), ethereumHandler::readAll);
+    }
+}
