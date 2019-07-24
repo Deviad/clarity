5d98531cf4b09e57ed49f31ae4dbc3c9dcc10935
Davide Pugliese <deviad@gmail.com>
2019-07-24 07:00:14 +0300

    Adding web3j socket

--- clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/services/InfuraSocketConfiguration.java
+++ clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/services/InfuraSocketConfiguration.java
@@ -1,8 +1,6 @@
 package com.clarity.transactiondispatcher.services;
 
 
-import io.reactivex.Flowable;
-import io.reactivex.disposables.Disposable;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.websocket.WebSocketClient;
 import org.web3j.protocol.websocket.WebSocketService;
-import org.web3j.protocol.websocket.events.NewHeadsNotification;
 
 import java.net.URI;
 import java.util.function.Consumer;
@@ -23,16 +20,36 @@ public class InfuraSocketConfiguration {
     @Autowired
     private Web3jService web3jService;
 
-    @Bean("webSocketSubscription")
+    @Bean("web3jSocketConnection")
     @SneakyThrows
-    Disposable webSocketSubscription() {
+    Web3j webSocketSubscription() {
         final WebSocketClient webSocketClient = new WebSocketClient(new URI(web3jService.getInfuraWsEndpoint()));
         final boolean includeRawResponses = false;
         final WebSocketService webSocketService = new WebSocketService(webSocketClient, includeRawResponses);
-
-        final Web3j web3j = Web3j.build(webSocketService);
-        final Flowable<NewHeadsNotification> notifications = web3j.newHeadsNotifications();
-        Consumer<String> loggerProducer = (String s) -> log.info("Logging web3j subscription: " + s);
-        return notifications.subscribe(x -> loggerProducer.accept(x.getJsonrpc()));
+        webSocketClient.connect();
+        return Web3j.build(webSocketService);
     }
+
+    private Consumer<String> logger() {
+        return (String s) -> log.info("Logging web3j subscription: " + s);
+    }
+//    @SneakyThrows
+//    @Bean("webSocketSubscription")
+//    Mono<Void> wsConnectNetty() {
+//        URI uri = new URI(web3jService.getInfuraWsEndpoint());
+//      return new ReactorNettyWebSocketClient().execute(uri,
+//                session -> session
+//                        .receive()
+//                        .map(WebSocketMessage::getPayloadAsText)
+////                        .take(MAX_EVENTS)
+//                        .doOnNext(txt -> log.info(session.getId() + ".IN: " + txt))
+//                        .flatMap(txt -> session.send(Mono.just(session.textMessage(txt))))
+//                        .doOnSubscribe(subscriber -> log.info(session.getId() + ".OPEN"))
+//                        .doFinally(signalType -> {session.close(); log.info(session.getId() + ".CLOSE"); })
+//                        .then()
+//
+//        );
+//    }
+
+
 }
--- clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/web/controller/EthereumAccountController.java
+++ clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/web/controller/EthereumAccountController.java
@@ -3,32 +3,44 @@ package com.clarity.transactiondispatcher.web.controller;
 
 import com.clarity.transactiondispatcher.services.EthereumOperations;
 import com.clarity.transactiondispatcher.services.PipelinrService;
+import com.clarity.transactiondispatcher.services.Web3jService;
 import com.clarity.transactiondispatcher.web.handler.EthereumAccountCreate;
 import com.clarity.transactiondispatcher.web.handler.EthereumAccountGetBalance;
 import com.clarity.transactiondispatcher.web.model.AccountBalanceRequestDTO;
 import com.clarity.transactiondispatcher.web.model.AccountRequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.java_websocket.util.Base64;
+import org.jooq.lambda.Unchecked;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.web3j.crypto.WalletFile;
+import org.web3j.protocol.Web3j;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class EthereumAccountController {
 
 
     private final PipelinrService pipelinrService;
     private final EthereumOperations operations;
+    @Qualifier("web3jSocketConnection") private final Web3j web3jSocketConnection;
+    private final Web3jService web3jService;
 
-    public EthereumAccountController(PipelinrService pipelinrService, EthereumOperations operations) {
-        this.pipelinrService = pipelinrService;
-        this.operations = operations;
-    }
 
     @PostMapping("/ethaccount")
     @SneakyThrows
@@ -45,6 +57,25 @@ public class EthereumAccountController {
     @PostMapping("/ethaccount/updatedbalance")
     @SneakyThrows
     public Mono<Map<String, Object>> getUpdatedAccountBalance(@RequestBody @Valid AccountBalanceRequestDTO accountBalanceRequestDTO) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        final CompletableFuture<Supplier<WalletFile>> supplierCompletableFuture = CompletableFuture
+                .supplyAsync(() -> Unchecked.supplier(
+                        () -> objectMapper.readValue(new String(Unchecked.supplier(
+                                () -> Base64.decode(accountBalanceRequestDTO.getWallet())).get()), WalletFile.class)));
+
+
+        web3jSocketConnection
+                .transactionFlowable()
+//                .map(transaction -> transaction.getTo())
+                .doOnSubscribe(
+                    subscription -> log.info("Subscribe to newly transactions confirmed on the blockchain."))
+                .filter(transaction-> transaction.getTo().equals(supplierCompletableFuture.get().get().getAddress()))
+                .subscribe(
+                        transaction-> log.info("Transaction from, to, gasPrice {}, {}, {}", transaction.getFrom(), transaction.getTo(), transaction.getGasPrice()),
+                        throwable -> log.error("Could not subscribe to block notifications: {}", throwable.getMessage())
+                );
         return pipelinrService.getQueryPipeline().send(new EthereumAccountGetBalance(accountBalanceRequestDTO, operations));
     }
 }
