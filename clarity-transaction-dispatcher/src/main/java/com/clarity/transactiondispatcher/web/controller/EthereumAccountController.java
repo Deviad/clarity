2bc16a56b10bc6bdf2a78b7d950a4ff8f3e66868
Davide Pugliese <deviad@gmail.com>
2019-07-28 10:06:00 +0300

    Milestone: get a stream of newHeads Newheads def: Fires a notification each time a new header is appended to the chain, including chain reorganizations. Users can use the bloom filter to determine if the block contains logs that are interested to them.

--- clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/web/controller/EthereumAccountController.java
+++ clarity-transaction-dispatcher/src/main/java/com/clarity/transactiondispatcher/web/controller/EthereumAccountController.java
@@ -1,34 +1,35 @@
 package com.clarity.transactiondispatcher.web.controller;
 
 
+import com.clarity.transactiondispatcher.services.EthereumClient;
 import com.clarity.transactiondispatcher.services.EthereumOperations;
 import com.clarity.transactiondispatcher.services.PipelinrService;
-import com.clarity.transactiondispatcher.services.Web3jService;
 import com.clarity.transactiondispatcher.web.handler.EthereumAccountCreate;
 import com.clarity.transactiondispatcher.web.handler.EthereumAccountGetBalance;
 import com.clarity.transactiondispatcher.web.model.AccountBalanceRequestDTO;
 import com.clarity.transactiondispatcher.web.model.AccountRequestDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.util.Base64;
 import org.jooq.lambda.Unchecked;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.socket.WebSocketMessage;
-import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.web3j.crypto.WalletFile;
-import org.web3j.protocol.Web3j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @Slf4j
@@ -38,9 +39,7 @@ public class EthereumAccountController {
 
     private final PipelinrService pipelinrService;
     private final EthereumOperations operations;
-    @Qualifier("web3jSocketConnection") private final Web3j web3jSocketConnection;
-    private final Web3jService web3jService;
-
+    private final EthereumClient ethereumClient;
 
     @PostMapping("/ethaccount")
     @SneakyThrows
@@ -66,16 +65,26 @@ public class EthereumAccountController {
                                 () -> Base64.decode(accountBalanceRequestDTO.getWallet())).get()), WalletFile.class)));
 
 
-        web3jSocketConnection
-                .transactionFlowable()
-//                .map(transaction -> transaction.getTo())
-                .doOnSubscribe(
-                    subscription -> log.info("Subscribe to newly transactions confirmed on the blockchain."))
-                .filter(transaction-> transaction.getTo().equals(supplierCompletableFuture.get().get().getAddress()))
-                .subscribe(
-                        transaction-> log.info("Transaction from, to, gasPrice {}, {}, {}", transaction.getFrom(), transaction.getTo(), transaction.getGasPrice()),
-                        throwable -> log.error("Could not subscribe to block notifications: {}", throwable.getMessage())
-                );
+//        web3jSocketConnection
+//                .transactionFlowable()
+////                .map(transaction -> transaction.getTo())
+//                .doOnSubscribe(
+//                    subscription -> log.info("Subscribe to newly transactions confirmed on the blockchain."))
+//                .filter(transaction-> transaction.getTo().equals(supplierCompletableFuture.get().get().getAddress()))
+//                .subscribe(
+//                        transaction-> log.info("Transaction from, to, gasPrice {}, {}, {}", transaction.getFrom(), transaction.getTo(), transaction.getGasPrice()),
+//                        throwable -> log.error("Could not subscribe to block notifications: {}", throwable.getMessage())
+//                );
         return pipelinrService.getQueryPipeline().send(new EthereumAccountGetBalance(accountBalanceRequestDTO, operations));
+    }
+
+    @GetMapping(value = "/ethaccount/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getTest() {
+        Map<String, Object> map = Stream.of(
+                new AbstractMap.SimpleEntry<>("id", 1),
+                new AbstractMap.SimpleEntry<>("method", "eth_subscribe"),
+                new AbstractMap.SimpleEntry<>("params", new Object[]{"newHeads"})
+        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        return Flux.from(ethereumClient.request(map));
     }
 }
