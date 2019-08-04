9b8cfaa008dad4f49e87c2eb97ba6b0cc1965726
Davide Pugliese <deviad@gmail.com>
2019-08-04 09:20:53 +0300

    Code cleanup

--- clarity-dispatcher/src/main/java/clarity/dispatcher/WebSocketListenerImpl.java
+++ clarity-dispatcher/src/main/java/clarity/dispatcher/WebSocketListenerImpl.java
@@ -5,22 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
-import okio.Buffer;
-import okio.BufferedSink;
 import okio.ByteString;
 import org.jooq.lambda.Unchecked;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -41,11 +32,7 @@ public class WebSocketListenerImpl extends WebSocketListener {
 
   @Override
   public void onMessage(WebSocket webSocket, String text) {
-    //            output("Receiving : " + text);
     log.info(text);
-
-    final Buffer buffer = new Buffer();
-
     Map<String, Object> map =
         Stream.of(
                 new AbstractMap.SimpleEntry<>("id", 1),
@@ -54,10 +41,8 @@ public class WebSocketListenerImpl extends WebSocketListener {
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     final ObjectMapper mapper = new ObjectMapper();
     String json = Unchecked.supplier(() -> mapper.writeValueAsString(map)).get();
-
     bus.setObject(text);
     webSocket.send(json);
-//    streamPrimesToSinkAsynchronously(buffer, json);
   }
 
   @Override
@@ -70,40 +55,4 @@ public class WebSocketListenerImpl extends WebSocketListener {
 
   @Override
   public void onFailure(WebSocket webSocket, Throwable t, Response response) {}
-
-  private void streamPrimesToSinkAsynchronously(final BufferedSink sink, String jsonString) {
-//    Thread thread =
-//        new Thread("writer") {
-//          @Override
-//          public void run() {
-//            try {
-//              for (int i = 2; i <= 997; i++) {
-//                Thread.sleep(10);
-//                sink.writeUtf8(jsonString);
-//              }
-//              sink.close();
-//
-//            } catch (IOException | InterruptedException e) {
-//              e.printStackTrace();
-//            }
-//          }
-//        };
-//    thread.start();
-
-//    final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-//    executorService.scheduleAtFixedRate(
-//            () -> Unchecked.runnable(() -> sink.writeUtf8(jsonString)).run(),
-//            1,
-//            1,
-//            TimeUnit.SECONDS);
-
-//    final Timer timer = new Timer("MyTimer");
-//    timer.schedule(new TimerTask() {
-//      @Override
-//      public void run() {
-//        Unchecked.runnable(() -> sink.writeUtf8(jsonString)).run();
-//      }
-//    }, 0, TimeUnit.SECONDS.toSeconds(1));
-
-  }
 }
